2cbe6c9f1d93d7b3e0e8aa019d94f08b8946ec1a
vfa-trungtt <trungtt@vitalify.asia>
2017-08-31 18:13:25 +0700

    update draw watermark on video

--- VFDemo/app/src/main/java/vfa/vfdemo/videoeditor/ActivityPlayMovie.java
+++ VFDemo/app/src/main/java/vfa/vfdemo/videoeditor/ActivityPlayMovie.java
@@ -4,16 +4,18 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import vfa.vfdemo.ActivitySlideMenu;
 import vn.hdisoft.hdilib.activities.VFActivity;
 
 /**
  * Created by trungtt on 8/31/17.
  */
 
-public class ActivityPlayMovie extends VFActivity {
+public class ActivityPlayMovie extends ActivitySlideMenu {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         FragVideoPlay fg = new FragVideoPlay();
         fg.setMovieFilePath(FragVideoAddWatermark.destPath);
         setRootFragment(fg);
