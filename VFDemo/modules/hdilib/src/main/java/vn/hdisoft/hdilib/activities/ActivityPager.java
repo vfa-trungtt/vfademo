fa93e121bdd91e1c79256a5cfb6c0fa55d84a426
vfa-trungtt <trungtt@vitalify.asia>
2017-09-08 16:20:35 +0700

    test tag

--- VFDemo/modules/hdilib/src/main/java/vn/hdisoft/hdilib/activities/ActivityPager.java
+++ VFDemo/modules/hdilib/src/main/java/vn/hdisoft/hdilib/activities/ActivityPager.java
@@ -1,8 +1,15 @@
 package vn.hdisoft.hdilib.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by trungtt on 9/8/17.
  */
 
-public class ActivityPager {
+public class ActivityPager extends VFActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
